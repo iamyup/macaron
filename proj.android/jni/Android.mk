cd7e1caabc821afe876eaa0e04f7dfee932debb0
yup <myname.is.yup@gmail.com>
2014-09-07 13:49:14 +0900

    안드로이드 프로젝트 파일 수정.

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,8 +12,10 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp \
-                   ../../Classes/GameLayer.cpp
+                   ../../Classes/LogoScene.cpp \
+                   ../../Classes/GameScene.cpp \
+                   ../../Classes/MacaronAnimLayer.cpp \
+                   ../../Classes/RapLayer.cpp 
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
